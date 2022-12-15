04953dcd27783250f4f96de68ea03f780001e687
Irvin Torres <54039997+Alexisrx96@users.noreply.github.com>
2022-12-14 21:53:28 -0600

    Add logger

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging
+
+
+logger = logging.getLogger('Pdf_viewer')
+format = logging.Formatter('%(asctime)s %(message)s')
+stream = logging.StreamHandler()
+file = logging.FileHandler("./logs.log")
+stream.setFormatter(format)
+stream.setLevel(logging.INFO)
+file.setFormatter(format)
+file.setLevel(logging.INFO)
+logger.setLevel(logging.INFO)
+logger.addHandler(stream)
+logger.addHandler(file)
