59db0ac476b8d85d5df3c8b36aa6b28cff66b166
Irvin Torres <54039997+Alexisrx96@users.noreply.github.com>
2022-12-14 22:09:11 -0600

    Add PDFViewerFrame

--- custom_elements.py
+++ custom_elements.py
@@ -1,6 +1,6 @@
 import math
 from dataclasses import dataclass, field
-from tkinter import Label, StringVar
+from tkinter import Frame, Label, PhotoImage, Scrollbar, StringVar, Text
 from tkinter.ttk import Progressbar
 
 from logger import logger
@@ -31,3 +31,39 @@ class ProgressbarElement:
         self.loading['value'] = percentage
         self.text.set("Please wait!, your pdf is loading "
                       f"{math.floor(percentage)}%")
+
+
+@dataclass
+class PDFViewerFrame:
+
+    frame: Frame
+    """Parent frame"""
+    pdf_location: str
+    """Path to PDF file"""
+    has_loading_bar: bool = field(default=True)
+    """Displays loading bar at the top of the parent frame"""
+
+    pdf_container: Text = field(init=False, default=None)
+    """Container for the PDF images"""
+    scrolls: tuple[Scrollbar, Scrollbar] = field(init=False,
+                                                 default_factory=tuple)
+    """Scroll bars for the PDF container"""
+    dimensions: tuple[int, int] = field(init=False)
+    """dimensions of the parent frame"""
+    progressbar: ProgressbarElement = field(init=False, default=None)
+    """Progressbar element"""
+    pages: list[PhotoImage] = field(default_factory=list, init=False)
+    """Store pdf images"""
+
+    def __post_init__(self):
+        self.dimensions = self.frame['width'], self.frame['height']
+
+    def forget(self):
+        """Remove progressbar and label."""
+        logger.info("Removing progressbar")
+        for scroll in self.scrolls:
+            scroll.destroy()
+        if self.pdf_container:
+            self.pdf_container.destroy()
+        if self.progressbar:
+            self.progressbar.forget()
