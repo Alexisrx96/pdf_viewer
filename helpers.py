4903434d507b5687f9f7351442b8f6c63032825b
Irvin Torres <irvinrx1996@hotmail.com>
2022-12-16 00:26:20 -0600

    Return a value between 0 and 1

--- helpers.py
+++ helpers.py
@@ -10,7 +10,7 @@ class ViewerHelper:
     def get_percentage(current_page_num: int, total: int) -> float:
         """Given a page number and a total number of pages,
         return the percentage"""
-        return current_page_num / float(total) * 100
+        return current_page_num / float(total)
 
     @staticmethod
     def extract_image(page: Page) -> PhotoImage:
